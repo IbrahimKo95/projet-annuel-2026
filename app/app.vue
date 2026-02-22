071edf0e482833b386503061f7af11dd9cbc4995
mathurin <Kimbembebobann2001@gmail.com>
2026-02-22 16:30:23 +0100

    feat(entitie): create all entities and manage db.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="h-full bg-gray-50">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
-import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from 'nuxt/config'
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
+  $development: undefined, $env: undefined, $meta: undefined, $production: undefined, $test: undefined,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@sidebase/nuxt-auth'],
+  typescript: {
+    strict: true,
+    shim: false
+  },
   css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
-  },
+  }
 })
